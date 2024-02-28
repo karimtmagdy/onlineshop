87dfb5b1ba915b4977bada90f21d8cffa41ae4fb
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2024-02-29 01:21:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const express = require("express")
 
 const app = express()
 
+
+/**
+* @desc   Profile Photo Upload
+* @route  /users/profile/profile-photo
+* @method GET
+* @access Private (only admin)
+*/
+
+
 app.liesnt(7000)
