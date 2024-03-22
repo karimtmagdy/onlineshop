020831efbe273e71e76e4813fb349637646c70f6
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2024-03-21 18:24:51 -0700

    Update auth.controller.js

--- auth.controller.js
+++ auth.controller.js
@@ -1,1 +1,9 @@
 import bcrypt from 'bcrypt'
+
+export register= warpper(async(req,res)=>{
+const {username,email, password}=req.body
+const existsUser.findOne({email},{'__v':false})
+if(existsUser){
+res.status(400).json({status:'fail',message:'some went wrong'})
+}
+})
