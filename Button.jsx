c91eada7320b5320a0af46ed2fffdc8e595f5662
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2024-05-14 07:34:25 -0700

    Update

---
+++ Button.jsx
@@ -0,0 +1,29 @@
+import cva from 'class-variance-authority'
+import {clsx} from 'clsx'
+import {twMarge} from 'tailwind-marge'
+
+const cva=([],{
+variants:{
+size:{},
+size:{},
+size:{},
+size:{},
+size:{},
+size:{},
+size:{},
+}
+})
+const Button =({,,,,,,,})=>{
+return (<>
+<button>
+{}{}{}
+</button>
+</>
+
+)
+}
+
+
+
+
+
